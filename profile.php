010c4cb1c07870f0ffc6db53fc436c4d66ae6478
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-09-30 21:35:48 +0530

    code updated

--- profile.php
+++ profile.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+    <!-- Profile -->
     <div class="profile">
         <h2>Welcome - <span><?php if(isset($_SESSION["user_email"])){ echo $_SESSION["user_fname"]; } ?></span></h2>
         <div class="data"><?php if(isset($_SESSION["user_email"])){ echo $_SESSION["user_fname"]." ".$_SESSION["user_lname"]; } ?></div>
